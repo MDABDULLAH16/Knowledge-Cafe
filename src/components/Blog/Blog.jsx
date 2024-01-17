a23fdf85ba22f14cf149197fd1ca4e07c7a105a3
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-17 23:46:12 +0600

    display all blogs

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Blog.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookBookmark, faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Blog = ({ blog }) => {
   const {
@@ -14,6 +16,27 @@ const Blog = ({ blog }) => {
   return (
     <div className="blog">
       <img src={banner_img} alt="" />
+      <div className="user-container flex">
+        <div className="flex ">
+          <img src={user_img} alt="" />
+          <div className="user-name">
+            <h3>{user_name}</h3>
+            <p>{post_time}</p>
+          </div>
+        </div>
+        <div className="flex read-time ">
+          <p>{post_read_time} min read</p>
+          <FontAwesomeIcon className="book-mark" icon={faBookmark} />
+        </div>
+      </div>
+      <div className="title">
+        <div>
+          <h1>{title}</h1>
+          <p>{tags}</p>
+        </div>
+        <a className="mark-as-read">Mark as read</a>
+      </div>
+      <hr />
     </div>
   );
 };
